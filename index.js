ce96c3a1cc1144b5abb2157dc3115ba7b4ac5c3a
RenatoCP <renato_pinha@icloud.com>
2021-12-04 13:56:51 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+
+let text = document.createTextNode('Renato is the champion')
+
+newHeader.appendChild(text)
+
